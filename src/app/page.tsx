d25008c915a9299928e638613ca91fa12915e3d3
alihassan2345 <ab42485557@gmail.com>
2026-01-19 05:13:11 -0800

    newcommit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,76 @@
-import Inbox from '@/Components/Inbox'
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+
 
 const page = () => {
+  const [task, setTask] = useState('')
+  const [status, setStatus] = useState('pending')
+
+
+  
+
   return (
     <div>
-      
-      <Inbox/>
+
+
+      <div className='flex justify-center mt-[50px]'>
+
+        <input className=' border-2 ' type="text" name="" id="" value={task} onChange={(e) => setTask(e.target.value)}/>
+
+
+
+        <select name="options" id="options" value={status} onChange={(e) => setStatus(e.target.value)}>
+          <option value="complete">complete</option>
+          <option value="pending">pending</option>
+          <option value="later">later</option>
+        </select>
+      </div>
+
+
+      <div>
+
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <div>
+
+
+      </div>
+
+
+
+
+      <div>
+
+
+      </div>
 
     </div>
   )
 }
 
 export default page
+
+
