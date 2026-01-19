61974a799a3b455c6cec52200b8da1435ec36838
alihassan2345 <ab42485557@gmail.com>
2026-01-19 04:43:39 -0800

    todo wrong

--- src/Components/Inbox.tsx
+++ src/Components/Inbox.tsx
@@ -1,38 +1,43 @@
+import { Ellipsis, InboxIcon, Megaphone, Menu } from 'lucide-react'
 import React from 'react'
 
 const Inbox = () => {
-  return (
-    <>
-        <div>
-            <div id='inboxtitle'>
-                <h3>Inbox</h3>
-                <div title='icon_hide'></div>
+    return (
+        <>
+            <div className='w-[350px] bg-amber-600 ml-[20px] mt-[100px] h-[400px] rounded-lg shadow-md '>
+                <div id='inboxtitle' className='flex justify-between bg-blue-900 pl-[15px] pr-[15px] pt-[10px] pb-[10px] rounded-t-lg  '>
+                    <div className='flex gap-[5px]'><InboxIcon /> <h3 className='text-white'> Inbox</h3></div>
+                    <div title='icon_hide' className='flex gap-[20px]'>
+                        <Megaphone />
+                        <Ellipsis />
+                        <Menu />
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                <div></div>
             </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            <div></div>
-        </div>
-
-    </>
-  )
+        </>
+    )
 }
 
 export default Inbox
